235aa495c14842b91b97e9dc79a748309e7eb76a
SageHendricks <89399536+SageHendricks@users.noreply.github.com>
2021-08-30 08:51:17 -0400

    removed, auto value

--- prime/ahhhhhhhhhhh.py
+++ prime/ahhhhhhhhhhh.py
@@ -1,7 +1,6 @@
 from math import floor
 try:
     user_input = int(input ("input a number to check if its prime \n> "))
-    user_input = 2**13 - 1
 except ValueError:
    print("youre not nice :grumpy face: goodbye.")
 else:
