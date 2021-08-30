2cb22fba7c0c20fc5972e18029dfc5bee8633215
SageHendricks <89399536+SageHendricks@users.noreply.github.com>
2021-08-30 08:50:46 -0400

    fixed prime gen, removed long percent

--- prime/ahhhhhhhhhhh.py
+++ prime/ahhhhhhhhhhh.py
@@ -7,7 +7,6 @@ except ValueError:
 else:
     output = "prime"
     for a in range(2, floor(pow(user_input,0.5))) :
-        print(f"{a} out of {floor(pow(user_input,0.5))}: {floor(a*100000000/floor(pow(user_input,0.5)))/1000000}%")
         if (user_input % a) == 0:
             output = "not prime " + str(a)
             break
